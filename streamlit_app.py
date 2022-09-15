da10267cda20c444a86738085693a02c7ef6a955
CaptRKirk3 <102709007+CaptRKirk3@users.noreply.github.com>
2022-09-15 15:49:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,8 @@ streamlit run my_app.py --server.enableCORS=false
 streamlit run my_app.py --server.enableWebsocketCompression=false
 
 # New section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
